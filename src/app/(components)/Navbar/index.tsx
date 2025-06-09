cf01dba8f0aad50ac15a90731c4f18d277a19ce9
Noi-Git <noi.patsin@gmail.com>
2025-06-08 22:15:06 -0400

    add info to left navigation bar

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,7 +1,24 @@
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Navbar = () => {
-  return <div className={`bg-amber-100`}>Navbar</div>
+  return (
+    <div className={`flex justify-between items-center w-full mb-7`}>
+      {/* LEFT SIDE NAV */}
+      <div className='flex justify-between items-center gap-5'>
+        <button
+          className={`px-3
+          bg-gray-100
+          rounded-full
+          hover:bg-blue-100`}
+          // onClick={() => {}}
+        >
+          <Menu className='w-4 h-4' />
+        </button>
+      </div>
+      {/* RIGHT SIDE NAV */}
+    </div>
+  )
 }
 
 export default Navbar
