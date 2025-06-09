32cafb674f56aad3cc5b2654b2f6d00b199c3252
Noi-Git <noi.patsin@gmail.com>
2025-06-08 22:44:12 -0400

    add more components to navbar

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,22 +1,46 @@
-import { Menu } from 'lucide-react'
+'use client'
+
+import { Bell, Menu, Sun } from 'lucide-react'
 import React from 'react'
+import { navSearch } from '../../../styles/navbar_style'
 
 const Navbar = () => {
   return (
     <div className={`flex justify-between items-center w-full mb-7`}>
       {/* LEFT SIDE NAV */}
+      {/* Menu */}
       <div className='flex justify-between items-center gap-5'>
         <button
           className={`px-3
           bg-gray-100
           rounded-full
           hover:bg-blue-100`}
-          // onClick={() => {}}
+          onClick={() => {}}
         >
           <Menu className='w-4 h-4' />
         </button>
       </div>
+      {/* Search bar */}
+      <div className='relative'>
+        <input
+          className={navSearch}
+          type='search'
+          placeholder='Search groups & products'
+        />
+        <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none:'>
+          <Bell className='text-gray-500' size={20} />
+        </div>
+      </div>
       {/* RIGHT SIDE NAV */}
+      <div className='flex justify-between items-center gap-5'>
+        <div className='hidden md:flex justify-between items-center gap-5'>
+          <div>
+            <button className='null'>
+              <Sun className='cursor-pointer text-gray-500' size={24} />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
