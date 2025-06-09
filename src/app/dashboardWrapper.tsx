7d4889340bc31efc47bec7929fe60bc70b72412c
Noi-Git <noi.patsin@gmail.com>
2025-06-08 20:58:53 -0400

    setup dashboard wrapper

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return <div>DashboardWrapper</div>
+}
+
+export default DashboardWrapper
